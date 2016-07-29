36ef03be49d87d1a622eaa0586a6351e2608e4f8
Jedrzej B <borowski.jedrzej@gmail.com>
2016-07-29 06:21:52 +0200

    SFX, win state, other tweaks

--- Backend/app/assets/javascripts/game_components/tower.js
+++ Backend/app/assets/javascripts/game_components/tower.js
@@ -2,6 +2,12 @@ GameClient.Tower = function(conflux, game, grandpasTower) {
   this.powerOn = false;
   this.sendingData = false;
   this.pluggedIn = false;
+
+  this.sfx = game.add.audio('comp_noise');
+  this.sfx.volume = 0;
+  this.sfx.loop = true;
+  this.sfx.play();
+
   if(grandpasTower.towerSwitches.powerOn == "left") {
     this.powerOnSetting = false;
   } else {
@@ -12,6 +18,15 @@ GameClient.Tower = function(conflux, game, grandpasTower) {
   } else {
     this.monitorSetting = true;
   }
+
+  this.update = function(){
+    if(this.powerOn){
+      this.sfx.volume = 0.5;
+    } else {
+      this.sfx.volume = 0;
+    }
+  }
+  
 }
 GameClient.Tower.prototype = Object.create(Phaser.Sprite.prototype);
 GameClient.Tower.prototype.constructor = GameClient.Tower;
