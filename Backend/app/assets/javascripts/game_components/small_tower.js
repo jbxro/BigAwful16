d9a76134b14fe51555e0be9d9c734006109dd319
GlyphGryph <glyphgryph@gmail.com>
2016-08-09 00:17:40 -0400

    Began text extraction from popups.

--- Backend/app/assets/javascripts/game_components/small_tower.js
+++ Backend/app/assets/javascripts/game_components/small_tower.js
@@ -7,7 +7,23 @@ GameClient.SmallTower = function(conflux, game, x, y, group, data) {
   this.game = game;
   this.conflux = conflux;
 
-  // non-clickable elements
+  // Text
+
+  this.zeroX = -1*(this.getLocalBounds().width * this.anchor.x);
+  this.zeroY = -1*(this.getLocalBounds().height * this.anchor.y);
+  // draw tower name
+  this.text1 = this.addChild(this.game.add.text(this.zeroX + this.width*0.5, this.zeroY + this.height*0.35, data.name+" Tower"));
+  this.text1.anchor.setTo(0.5);
+  this.text1.fontSize = 25;
+  // draw cable icon
+  this.cable = this.addChild(this.game.make.sprite(this.zeroX + this.width/2, this.zeroY + this.height*0.55, 'cableIcons'));
+  this.cable.anchor.setTo(0.5);
+  // set cable color
+  this.cable.frame = this.conflux.colors[data.towerCable];
+  // draw cable description
+  this.text2 = this.addChild(this.game.add.text(this.zeroX + this.width/2, this.zeroY + this.height*0.7, "Power cable"));
+  this.text2.anchor.setTo(0.5);
+  this.text2.fontSize = 20;
 
   // draw round buttons
   for(var i=0;i<data.roundButtons.length;i++){
@@ -27,8 +43,6 @@ GameClient.SmallTower = function(conflux, game, x, y, group, data) {
     }
   }
 
-  // clickable elements
-
   // draw switches
   for(var i=0;i<conflux.switchCoordinates.length;i++){
     var coords = conflux.switchCoordinates[i];
@@ -40,6 +54,7 @@ GameClient.SmallTower = function(conflux, game, x, y, group, data) {
     }
     var aSwitch = new GameClient.Element(conflux, game, coords[0], coords[1], game.world, 'switch', game.rnd.integerInRange(0, 1), desc);
     this.addChild(aSwitch);
+
   }
 
   for(var i=0;i<4;i++){
@@ -55,13 +70,13 @@ GameClient.SmallTower = function(conflux, game, x, y, group, data) {
   }
 
   // draw description pop up
-  this.addBubble = function(){
-    if(!this.displayingHelp){
-      var bubble = new GameClient.Bubble(this.game, this, "tower", data, "");
-      this.displayingHelp = true;
-    }
-  }
-  this.events.onInputDown.add(this.addBubble, this);
+  // this.addBubble = function(){
+  //   if(!this.displayingHelp){
+  //     var bubble = new GameClient.Bubble(this.game, this, "tower", data, "");
+  //     this.displayingHelp = true;
+  //   }
+  // }
+  // this.events.onInputDown.add(this.addBubble, this);
 }
 GameClient.SmallTower.prototype = Object.create(Phaser.Sprite.prototype);
 GameClient.SmallTower.prototype.constructor = GameClient.SmallTower;
