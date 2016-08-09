e34a6d8d1df316fb0db5712821ea6f3338018c8c
GlyphGryph <glyphgryph@gmail.com>
2016-08-08 23:00:14 -0400

    Split grandson client in two

--- Backend/app/assets/javascripts/game_client.js
+++ Backend/app/assets/javascripts/game_client.js
@@ -1,16 +1,17 @@
 GameClient = {
   start: function(gameType, id, data, target){
     var game = new Phaser.Game(1200, 700, Phaser.CANVAS, document.getElementById(target));
-    game.state.add('GrandpaGame', GameClient.GFG.GrandpaGame);
-    game.state.add('GrandsonGame', GameClient.GFG.GrandsonGame);
     switch (gameType) {
       case 'Grandpa':
+        game.state.add('GrandpaGame', GameClient.GFG.GrandpaGame);
         game.state.start('GrandpaGame', false, true, data);
         break;
       case 'Grandson':
         if(target == 'monitor'){
+          game.state.add('GrandsonMonitors', GameClient.GFG.GrandsonMonitors);
           game.state.start('GrandsonMonitors', false, true, data);
         }else{
+          game.state.add('GrandsonTowers', GameClient.GFG.GrandsonTowers);
           game.state.start('GrandsonTowers', false, true, data);
         }
         break;
